8150ff16265c789911663745196093c2fbef4940
Yim <ysj970828@gmail.com>
2024-05-16 00:25:42 +0900

    update

---
+++ script.js
@@ -0,0 +1,26 @@
+const modal = document.getElementById('modal');
+const openModal = document.querySelector('.open-button');
+const closeModal = document.querySelector('.close-button');
+const parent = document.getElementById('parentHTML');
+
+openModal.addEventListener('click', () => {
+    parent.classList.remove('modal-is-closing');
+    setTimeout(() => {
+        parent.classList.add('modal-is-opening');
+        parent.classList.add('modal-is-open');
+        modal.showModal(); 
+        
+    }, 200);
+})
+
+closeModal.addEventListener('click', () => {
+    parent.classList.remove('modal-is-opening');
+    parent.classList.remove('modal-is-open');
+    setTimeout(() => {
+        parent.classList.add('modal-is-closing');
+    }, 200);
+
+    setTimeout(() => {
+        modal.close(); 
+    }, 800);
+})
